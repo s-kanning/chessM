506a17b7b03bde929de62f44f9e69a20ebe5a4aa
Scott Kanning <scott.kanning@gmail.com>
2023-07-26 18:19:33 +0300

    added colors to palette

--- configurations.py
+++ configurations.py
@@ -1,4 +1,3 @@
-from PIL import ImageTk, Image
 # color palette
 
 Blue = "#1C315E"
@@ -6,9 +5,14 @@ Blue_green = "#227C70"
 Green = "#88A47C"
 Light_green = "#E6E2C3"
 Pink = "#E98EAD"
+Alt_light_green = "#B4C4AA"
 
 Highlight_color = '#afd548'
 
 
 #Font
 Font = "Roboto"
+
+
+
+# other: #B4C4AA, #86A696, #608784
